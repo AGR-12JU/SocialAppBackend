f7d2451f25f68cb05b8aa996b12def63a763d281
Aruj Govind Rao <agrao2000@gmail.com>
2024-12-29 14:29:47 +0530

    First commit

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express=require("express");
 const app =express();
 const cors=require("cors")
 const path=require("path")
-// const dotenv=require("dotenv");
+
 const mongoose=require('mongoose');
-// const {mongoUrl}=require("./keys");
+
 require('dotenv').config();
 require("./models/model");
 require("./models/post");
@@ -20,7 +20,7 @@ const port=process.env.port || 5000;
 const mongoUrl=`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.flgu4xq.mongodb.net/?retryWrites=true&w=majority`
 mongoose.connect(mongoUrl);
 mongoose.connection.on("connected",()=>{
-    console.log("Successfully connected");
+    console.log("Successfully connected")
 })
 mongoose.connection.on("error",()=>{
     console.log("Error in Connection");
